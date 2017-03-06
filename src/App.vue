58ab177b686bd13c48abd3fcfb5fc7c3f2f28ca8
Abbas Dharamsey <adharams@ucsd.edu>
2017-03-06 15:08:37 -0800

    pretty messy rn

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style src="./assets/css/materialize.min.css"></style>
+<style src="./assets/css/styles.css"></style>
+
---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Lead from '@/components/Lead'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Lead',
+      component: Lead
+    }
+  ]
+})
