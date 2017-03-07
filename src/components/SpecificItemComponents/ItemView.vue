d604efcacc67daf18c7785dc56a4a24a4690cc9f
Abbas Dharamsey <adharams@ucsd.edu>
2017-03-06 22:50:12 -0800

    please leave it

---
+++ src/components/SpecificItemComponents/ItemView.vue
@@ -0,0 +1,17 @@
+ <template>
+  <div class="col s6">
+    <h1>FUCKING CHINKS</h1>
+    <h5>{{itemBrand}}</h5>
+    <p>{{itemName}}</p>
+    <img alt="" :src="itemPictureSource" class="responsive-img">
+    <a class="waves-effect waves-light btn">Add to Collection</a>
+    <a class="waves-effect waves-light btn">Add to Wish List</a>
+  </div>
+</template> 
+
+<script>
+  export default {
+    name: 'ItemView',
+    props: ['itemBrand', 'itemName', 'itemPictureSource']
+  }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Lead from '@/components/Lead'
+import SpecificItem from '../components/SpecificItem'
 
 Vue.use(Router)
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'Lead',
       component: Lead
+    },
+    {
+      path: '/ayy',
+      name: 'Item',
+      component: SpecificItem
     }
   ]
 })
