bd535ae2d580d37be7a1ebac6ab21dae96cd6ca5
Abbas Dharamsey <adharams@ucsd.edu>
2017-03-06 16:40:13 -0800

    god damn c*

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <div id="app">
+    <Nav></Nav>
     <router-view></router-view>
   </div>
 </template>
 
+
 <script>
+import Nav from './components/Nav'
 export default {
+  components: {
+    Nav
+  },
   name: 'app'
 }
 </script>
 
 <style src="./assets/css/materialize.min.css"></style>
 <style src="./assets/css/styles.css"></style>
-
--- src/components/Lead.vue
+++ src/components/Lead.vue
@@ -1,32 +1,52 @@
-<template>
-  <div>
-  <nav>
-    <div id="header" class="nav-wrapper">
-    
-          <a href="#" class="brand-logo">PopDex!&nbsp;&nbsp;<small>The Definitive Guide!</small></a>
-          <ul class="right hide-on-med-and-down">
-            <li><a href="wireframes/loginForm.html">Login</a></li>
-            <li><a href="wireframes/signupForm.html">Signup</a></li>
-          </ul>
-    </div>
-  </nav>
+  <template>
+  <div id="content" class="container">
 
-  <div id="navigator">
-    <a href="">The Guide</a>
-    <a href="">My Stuff</a>
-    <a href="">News</a>
-  </div>
-  </div>
+      <div class="row">
+        <div class="col s12">
+          <div id="preContent">
+            <h4>Trending Pops</h4>
+          </div>
+        </div>
+        
+        <div id=mainContent>
+        
+          <div class="col s4">
+            <div class="frontPagePopFrame">
+              <div class=imageCropper><img alt="" src="wireframes/img/wolf.jpg" width="266" height="200" class="circle"></div>
+              <div class=frontPagePopFrameText>
+                <p class=frontPagePopUniverse>Marvell</p>
+                <p class=frontPagePopName>Wolverine</p>
+              </div>
+            </div>
+          </div>
+        
+          <div class="col s4">
+            <div class="frontPagePopFrame">
+              <div class=imageCropper><img alt="" src="wireframes/img/wolf.jpg" width="266" height="200" class="circle"></div>
+              <div class=frontPagePopFrameText>
+                <p class=frontPagePopUniverse>Marvell</p>
+                <p class=frontPagePopName>Wolverine</p>
+              </div>
+            </div>
+          </div>
+        
+          <div class="col s4">
+            <div class="frontPagePopFrame">
+              <div class=imageCropper><img alt="" src="wireframes/img/wolf.jpg" width="266" height="200" class="circle"></div>
+              <div class=frontPagePopFrameText>
+                <p class=frontPagePopUniverse>Marvell</p>
+                <p class=frontPagePopName>Wolverine</p>
+              </div>
+            </div>
+                  </div>
+          </div>
+      </div>
 
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'lead',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
+  name: 'Lead'
 }
 </script>
