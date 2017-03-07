39f3eea314e379c7ef834c28620658c223c4d913
Abbas Dharamsey <adharams@ucsd.edu>
2017-03-06 17:51:32 -0800

    that's bad

--- src/components/Lead.vue
+++ src/components/Lead.vue
@@ -1,52 +1,55 @@
   <template>
-  <div id="content" class="container">
+    <div id="content" class="container">
 
-      <div class="row">
-        <div class="col s12">
-          <div id="preContent">
-            <h4>Trending Pops</h4>
+        <div class="row">
+          <div class="col s12">
+            <div id="preContent">
+              <h4>Trending Pops</h4>
+            </div>
+          </div>
+          
+          <div id="mainContent">
+            <FrontPage 
+              v-for="item in items" 
+              v-bind:itemBrand="item.itemBrand" 
+              v-bind:itemName="item.itemName" 
+              v-bind:itemPictureSource="item.itemPictureSource">
+            </FrontPage>
           </div>
         </div>
-        
-        <div id=mainContent>
-        
-          <div class="col s4">
-            <div class="frontPagePopFrame">
-              <div class=imageCropper><img alt="" src="wireframes/img/wolf.jpg" width="266" height="200" class="circle"></div>
-              <div class=frontPagePopFrameText>
-                <p class=frontPagePopUniverse>Marvell</p>
-                <p class=frontPagePopName>Wolverine</p>
-              </div>
-            </div>
-          </div>
-        
-          <div class="col s4">
-            <div class="frontPagePopFrame">
-              <div class=imageCropper><img alt="" src="wireframes/img/wolf.jpg" width="266" height="200" class="circle"></div>
-              <div class=frontPagePopFrameText>
-                <p class=frontPagePopUniverse>Marvell</p>
-                <p class=frontPagePopName>Wolverine</p>
-              </div>
-            </div>
-          </div>
-        
-          <div class="col s4">
-            <div class="frontPagePopFrame">
-              <div class=imageCropper><img alt="" src="wireframes/img/wolf.jpg" width="266" height="200" class="circle"></div>
-              <div class=frontPagePopFrameText>
-                <p class=frontPagePopUniverse>Marvell</p>
-                <p class=frontPagePopName>Wolverine</p>
-              </div>
-            </div>
-                  </div>
-          </div>
-      </div>
 
-  </div>
+    </div>
 </template>
 
 <script>
+import FrontPage from './LeadComponents/FrontPagePopComponent'
+
 export default {
-  name: 'Lead'
+  components: {
+    FrontPage
+  },
+  name: 'Lead',
+  data () {
+    return {
+      // get object from server and render FrontPagePopComponent
+      items: [
+        {
+          itemBrand: 'Abbas',
+          itemName: 'No',
+          itemPictureSource: 'http://globalgamejam.org/sites/default/files/styles/game_sidebar__normal/public/game/featured_image/promo_5.png?itok=9dymM8JD'
+        },
+        {
+          itemBrand: 'Abbas',
+          itemName: 'No',
+          itemPictureSource: 'http://globalgamejam.org/sites/default/files/styles/game_sidebar__normal/public/game/featured_image/promo_5.png?itok=9dymM8JD'
+        },
+        {
+          itemBrand: 'Abbas',
+          itemName: 'No',
+          itemPictureSource: 'http://globalgamejam.org/sites/default/files/styles/game_sidebar__normal/public/game/featured_image/promo_5.png?itok=9dymM8JD'
+        }
+      ]
+    }
+  }
 }
 </script>
---
+++ src/components/LeadComponents/FrontPagePopComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="col s4">
+    <div class="frontPagePopFrame">
+      <div class=imageCropper><img alt="" :src="itemPictureSource" width="266" height="200" class="circle"></div>
+      <div class=frontPagePopFrameText>
+        <p class=frontPagePopUniverse>{{itemBrand}}</p>
+        <p class=frontPagePopName>{{itemName}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'FrontPagePopComponent',
+    props: ['itemBrand', 'itemName', 'itemPictureSource']
+  }
+</script>
